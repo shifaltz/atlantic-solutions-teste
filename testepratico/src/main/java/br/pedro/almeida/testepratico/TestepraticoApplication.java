77a33ba69e2d8ae8a1cc8d5cff25fc1fd035ddbd
PedroDeAlmeidaSantos <pedroalmeidasantos.00@gmail.com>
2023-06-26 10:32:53 -0300

    correcao nome class

--- testepratico/src/main/java/br/pedro/almeida/testepratico/TestepraticoApplication.java
+++ testepratico/src/main/java/br/pedro/almeida/testepratico/TestepraticoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestePraticoApplication {
+public class TestepraticoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestePraticoApplication.class, args);
+		SpringApplication.run(TestepraticoApplication.class, args);
 	}
 
 }
