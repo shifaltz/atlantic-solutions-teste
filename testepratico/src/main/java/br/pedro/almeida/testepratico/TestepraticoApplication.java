5a63bf606c8429d5a70158e79520b9a2304837ed
PedroDeAlmeidaSantos <pedroalmeidasantos.00@gmail.com>
2023-06-26 02:39:39 -0300

    Correcao nome - Atlantic, v1

--- testepratico/src/main/java/br/pedro/almeida/testepratico/TestepraticoApplication.java
+++ testepratico/src/main/java/br/pedro/almeida/testepratico/TestepraticoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestepraticoApplication {
+public class TestePraticoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestepraticoApplication.class, args);
+		SpringApplication.run(TestePraticoApplication.class, args);
 	}
 
 }
